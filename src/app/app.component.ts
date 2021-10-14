cc460c4589e04173e1c840cfb3d122cca884d210
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-14 11:37:22 +0100

    value changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  newPost = 'Testing'
+
+  changePost() {
+    this.newPost = 'Changed input'
+  }
 }
